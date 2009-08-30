c819b4b7e9c4eacb12b43cb40d6f70e4da19a0d5
vladozar <vladozar@30b12fd5-35e0-4017-9073-2e4a06446cf5>
2009-08-30 13:00:58 +0000

    changed version to beta

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     }
     
     SoftProjector w;
-    w.setWindowTitle("softProjector 0.1.10");
+    w.setWindowTitle("softProjector 0.9.Beta01");
     w.show();
     return a.exec();
 }
