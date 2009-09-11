57232d557445c30e2a35e26f108de9b002506122
matvey83 <matvey83@30b12fd5-35e0-4017-9073-2e4a06446cf5>
2009-09-11 16:27:00 +0000

    Minor code rearrangement.

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,7 @@ int main(int argc, char *argv[])
     QApplication::setStyle("Plastique");
 
     bool fileExist = QFile::exists("spData.sqlite");
-    if (fileExist)
-    {
-        if( !connect() )
-            return 1;
-    }
-    else
+    if (!fileExist)
     {
         QMessageBox mb;
         mb.setText("Database file 'spData.sqlite' cannot be found.\nThe program will Terminate!");
@@ -45,7 +40,17 @@ int main(int argc, char *argv[])
         mb.exec();
         return 1;
     }
-    
+    if( !connect() )
+    {
+        QMessageBox mb;
+        mb.setText("Failed to connect to database 'spData.sqlite'");
+        mb.setWindowTitle("Database File Error");
+        mb.setIcon(QMessageBox::Critical);
+        mb.exec();
+        return 1;
+    }
+
+    // Connected to the database OK:
     SoftProjector w;
     w.setWindowTitle("softProjector 0.9.Alpha02");
     w.show();
