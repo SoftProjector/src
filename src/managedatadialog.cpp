8a14ccbe1d9050fb798761ee8e496350ac43b628
vladozar <vladozar@30b12fd5-35e0-4017-9073-2e4a06446cf5>
2009-09-13 13:17:47 +0000

    Added some classes for Sborniks and Sborniks are now shown on the dialog; swapped QTableWidget with QTableView;

--- src/managedatadialog.cpp
+++ src/managedatadialog.cpp
@@ -3,14 +3,23 @@
 
 ManageDataDialog::ManageDataDialog(QWidget *parent) :
     QDialog(parent),
-    m_ui(new Ui::ManageDataDialog)
+    ui(new Ui::ManageDataDialog)
 {
-    m_ui->setupUi(this);
+    ui->setupUi(this);
+    QList<Sbornik> sbor;
+    ManageDb db;
+    sbor = db.getSborniks();
+
+    SborniksModel *sb;
+    sb = new SborniksModel;
+    sb->setSbornik(sbor);
+    ui->sbornikTableView->setModel(sb);
+
 }
 
 ManageDataDialog::~ManageDataDialog()
 {
-    delete m_ui;
+    delete ui;
 }
 
 void ManageDataDialog::changeEvent(QEvent *e)
@@ -18,9 +27,149 @@ void ManageDataDialog::changeEvent(QEvent *e)
     QDialog::changeEvent(e);
     switch (e->type()) {
     case QEvent::LanguageChange:
-        m_ui->retranslateUi(this);
+        ui->retranslateUi(this);
         break;
     default:
         break;
     }
+}
+
+//***************************************
+//****        Sbornik                ****
+//***************************************
+Sbornik::Sbornik()
+{
+}
+
+//Sbornik::Sbornik(QString id)
+//{
+//    sbornikId = id;
+//}
+//
+//void Sbornik::readData()
+//{
+//    QSqlQuery sq;
+//    sq.exec("SELECT id, name, info FROM Sborniks WHERE id = '" + sbornikId + "'");
+//    sq.first();
+//    title = sq.value(1).toString();
+//    info = sq.value(2).toString();
+//}
+
+//***************************************
+//****        Bibles                ****
+//***************************************
+
+//***************************************
+//****        Sborniks Model         ****
+//***************************************
+SborniksModel::SborniksModel()
+{
+}
+
+void SborniksModel::setSbornik(QList<Sbornik> sborniks)
+{
+    sbornik_list.clear();
+    for (int i(0); i < sborniks.size(); i++)
+    {
+        Sbornik sbornik = sborniks.at(i);
+        sbornik_list.append(sbornik);
+    }
+    emit layoutChanged();
+}
+
+void SborniksModel::addSbornik(Sbornik sbornik)
+{
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
+    sbornik_list.append(sbornik);
+    endInsertRows();
+}
+
+Sbornik SborniksModel::getSbornik(int row)
+{
+    return sbornik_list.at(row);
+}
+
+int SborniksModel::rowCount(const QModelIndex &parent) const
+{
+    return sbornik_list.count();
+}
+
+int SborniksModel::columnCount(const QModelIndex &parent) const
+{
+    return 3;
+}
+
+QVariant SborniksModel::data(const QModelIndex &index, int role) const
+{
+    if( index.isValid() && role == Qt::DisplayRole )
+    {
+        Sbornik sbornik = sbornik_list.at(index.row());
+        if( index.column() == 0 )
+            return QVariant(sbornik.sbornikId);
+        else if( index.column() == 1 )
+            return QVariant(sbornik.title);
+        else
+            return QVariant(sbornik.info);
+    }
+    else
+        return QVariant();
+}
+
+QVariant SborniksModel::headerData(int section,
+                                 Qt::Orientation orientation,
+                                 int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal )
+    {
+        switch(section) {
+        case 0:
+            return QVariant("Id");
+        case 1:
+            return QVariant("Title");
+        case 2:
+            return QVariant("Info");
+        }
+    }
+    return QVariant();
+}
+
+bool SborniksModel::removeRows(int row, int count, const QModelIndex &parent)
+{
+    beginRemoveRows(parent, row, row+count-1);
+    // Need to remove starting from the end:
+    for(int i=row+count-1; i>=row; i--)
+        sbornik_list.removeAt(i);
+    endRemoveRows();
+    return true;
+}
+
+//***************************************
+//****        BiblesModel                ****
+//***************************************
+
+//***************************************
+//****        ManageDb                ****
+//***************************************
+ManageDb::ManageDb()
+{
+}
+
+QList<Sbornik> ManageDb::getSborniks()
+{
+    QList<Sbornik> sborniki;
+    Sbornik sbornik;
+    QSqlQuery sq;
+    sq.exec("SELECT id, name, info FROM Sborniks");
+    while (sq.next())
+    {
+        sbornik.sbornikId = sq.value(0).toString();
+        sbornik.title = sq.value(1).toString();
+        sbornik.info = sq.value(2).toString();
+        sborniki.append(sbornik);
+    }
+    return sborniki;
+}
+
+QList<Bibles> ManageDb::getBibles()
+{
 }
--- src/managedatadialog.h
+++ src/managedatadialog.h
@@ -2,6 +2,8 @@
 #define MANAGEDATADIALOG_H
 
 #include <QtGui/QDialog>
+#include <QtGui>
+#include <QtSql>
 
 namespace Ui {
     class ManageDataDialog;
@@ -18,7 +20,78 @@ protected:
     virtual void changeEvent(QEvent *e);
 
 private:
-    Ui::ManageDataDialog *m_ui;
+    Ui::ManageDataDialog *ui;
+};
+
+///////////////////////
+// Sbornik Class
+///////////////////////
+class Sbornik
+{
+public:
+    Sbornik();
+//    Sbornik(QString id);
+//    void readData();
+    QString sbornikId;
+    QString title;
+    QString info;
+};
+
+///////////////////////
+// Bibles Class
+///////////////////////
+class Bibles
+{
+    QString bibleId;
+    QString title;
+};
+
+///////////////////////
+// Sborniks Model Class
+///////////////////////
+class SborniksModel : public QAbstractTableModel
+//  Class for storing data for Sbornik Table
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(SborniksModel)
+
+public:
+    SborniksModel();
+    QList<Sbornik> sbornik_list;
+
+    void setSbornik(QList<Sbornik> sborniks);
+    void addSbornik(Sbornik sbornik);
+    Sbornik getSbornik(int row);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex());
+
+};
+
+///////////////////////
+// Bibles Model Class
+///////////////////////
+class BiblesModel : public QAbstractTableModel
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(BiblesModel)
+
+public:
+    BiblesModel();
+};
+
+///////////////////////
+// Database Class
+///////////////////////
+class ManageDb
+{
+public:
+    ManageDb();
+    QList<Sbornik> getSborniks();
+    QList<Bibles> getBibles();
 };
 
 #endif // MANAGEDATADIALOG_H
